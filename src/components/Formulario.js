9e36f829c611a58598ef144e9c6e7333fb152345
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-20 17:47:43 +0200

    Error comp

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Error from "./Error";
 
 const Formulario = () => {
+  const [termino, guardarTermino] = useState("");
+  const [error, guardarError] = useState(false);
+
+  const buscarImagenes = (e) => {
+    e.preventDefault();
+
+    // Validación
+    if (termino.trim() === "") {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+    // Enviar el término de búsqueda hacia el componente principal
+  };
+
   return (
-    <form>
+    <form onSubmit={buscarImagenes}>
       <div className="row">
         <div className="form-group col-md-9">
           <input
             type="text"
             className="form-control form-control-lg"
             placeholder="Find a image"
+            onChange={(e) => guardarTermino(e.target.value)}
           />
         </div>
         <div className="form-group col-md-3">
@@ -19,6 +36,8 @@ const Formulario = () => {
           />
         </div>
       </div>
+
+      {error ? <Error mensaje="Debes agregar un término de búsqueda." /> : null}
     </form>
   );
 };

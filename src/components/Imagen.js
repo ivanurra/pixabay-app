f0472288cff17a3bac6aa61e2a9a150fd811d00a
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-21 12:10:35 +0200

    see images

--- src/components/Imagen.js
+++ src/components/Imagen.js
@@ -5,7 +5,16 @@ const Imagen = ({ imagen }) => {
   return (
     <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
       <div className="card">
-        <img src={previewURL} alt={tags} className="card-img-top" />
+          <img src={previewURL} alt={tags} className="card-img-top" />
+        <div className="card-body">
+        <p className="card-text">Likes: {likes} &#10084;</p>
+        <p className="card-text">Views: {views}</p>
+        </div>
+        <div className="card-footer">
+        <a href={largeImageURL} target="_blank" rel="noreferrer" className="btn btn-primary btn-block">
+          Link to Image
+        </a>
+        </div>
       </div>
     </div>
   );
